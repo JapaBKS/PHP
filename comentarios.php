7c07dc85b4db431f93f176fde1e5ab92d02ffc6a
Bruno Sakaguchi <108704757+JapaBKS@users.noreply.github.com>
2024-09-20 00:53:15 -0300

    php

---
+++ comentarios.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Curso PHP</title>
+    </head>
+    <body>
+        <!-- Comentário em HTML --->
+        <?php 
+
+            //comentário de uma linha
+            echo 'Comando Echo <br>';
+
+            #comentário de uma linha
+            print 'Comando Print <hr>'
+
+            /*
+                Comentário de múltiplas linhas.
+                Este tipo de comentário permite que várias linhas sejam comentadas ao mesmo tempo.
+            */
+        ?>
+    </body>
+</html>
